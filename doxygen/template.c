dc63701a992d09921d83a1690b017d452aa4457a
Dave Chassin <dchassin@slac.stanford.edu>
2009-04-26 15:34:30 +0000

    Added doxygen, mingw, inno, and gnuplot setup tools

---
+++ doxygen/template.c
@@ -0,0 +1,9 @@
+/** $Id: template.c,v 1.1 2007/10/16 19:00:14 d3g637 Exp $
+	@file
+	@author David P. Chassin
+	@addtogroup 
+	@ingroup core
+ @{
+ **/
+
+/**@}*/
---
+++ doxygen/template.h
@@ -0,0 +1,8 @@
+/** $Id: template.h,v 1.1 2007/10/16 19:00:14 d3g637 Exp $
+	@file
+	@addtogroup
+	@ingroup core
+ @{
+ **/
+
+/**@}*/
