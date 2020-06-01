344651be33d5e388e8312a2c382bdfaeba242c73
Xie <jing.xie@pnnl.gov>
2020-06-01 15:52:47 -0700

    Update the functionality that plots the voltage magnitude difference between the two nodes connected via a specified switch under different reactive power injection of the PV.

--- python_scripts/CsvExtractor/extractor_csv.py
+++ python_scripts/CsvExtractor/extractor_csv.py
@@ -1,12 +1,12 @@
 # ***************************************
 # Author: Jing Xie
 # Created Date: 2020-5-1
-# Updated Date: 2020-5-25
+# Updated Date: 2020-6-1
 # Email: jing.xie@pnnl.gov
 # ***************************************
 
 import pandas as pd
-import seaborn as sns
+# import seaborn as sns
 import matplotlib.pyplot as plt
 
 import re
@@ -30,6 +30,8 @@ class CsvExt:
         self.nd_volt_v_df = None
         self.nd_volt_mag_v_df = None
         self.nd_delta_volt_mag_v_df = None
+        
+        self.swt_dv_df = None
 
         # ==Series
         self.pv_q_pu_ser = None
@@ -62,7 +64,7 @@ class CsvExt:
         self.pv_q_var_ser = self.csv_df.iloc[:, 1]
         
         ind_q_zero = pd.Index(self.pv_q_pu_ser).get_loc(0)
-
+        
         #== V & Delta V
         self.nd_volt_v_df = self.csv_df.iloc[:, 2 : 2 + num_nds * num_phs]
         self.nd_volt_mag_v_df = pd.DataFrame()
@@ -73,7 +75,6 @@ class CsvExt:
             self.nd_delta_volt_mag_v_df[cur_col] = self.nd_volt_mag_v_df[cur_col].apply(lambda x: x - self.nd_volt_mag_v_df.loc[ind_q_zero,cur_col])
         
     def plot_dq_dv(self, fig_fmt_str='.svg', fmt_dic = {'fontname':'Times New Roman','size': 16}):
-        # plt.figure(figsize=(16,6))
         plt.plot(self.pv_q_var_ser, self.nd_delta_volt_mag_v_df)        
         
         plt.title(self.csv_file_name, **fmt_dic)
@@ -96,10 +97,9 @@ class CsvExt:
         self.plot_dq_dv()
 
     def get_price_q_curve(self, coef_rt = 0.1, plot_flag = False, saved_fig_pref_str = 'p_q_', fig_fmt_str='.svg', fmt_dic = {'fontname':'Times New Roman','size': 16}):
-        """
-        Supply curve equation: Price = C_RT * [ (S_Inv^2 – Q^2) – ( S_Inv^2 – (Q+Q_Ofr)^2 ) ]
-        where S_Inv is the rating of the inverter, Q = initial operating point (Q) of the inverter that is taken as 0 for now,
-        and Q_Ofr = offered reactive power which is basically the one you vary. For now, please C_RT = 0.1 . This equation should give Price-reactive curve for the inverter which should be converted to voltage-reactive power using the sensitivity you obtained.
+        """ 
+        1) $/h or $? 
+        2) coef_rt is with the unit of $/kWh
         """
         self.pv_price_dollar_ser = coef_rt * self.pv_q_var_ser.pow(2)
 
@@ -124,6 +124,44 @@ class CsvExt:
             plt.savefig(fig_fpn)
             plt.show()
 
+    def get_dv_swt(self, dict_swt, list_ph_str=['voltage_A','voltage_B','voltage_C'], delim_str=':'):
+        self.swt_dv_df = pd.DataFrame()
+        for cur_key, cur_list in dict_swt.items():
+            for cur_ph in list_ph_str:
+                cur_col_suff_str = delim_str + cur_ph
+                self.swt_dv_df[cur_key+cur_col_suff_str] = abs(self.nd_volt_mag_v_df[cur_list[0]+cur_col_suff_str]
+                               - self.nd_volt_mag_v_df[cur_list[1]+cur_col_suff_str])
+    
+    def filter_line_slope(self, ls_th=1):
+        self.swt_dv_df = self.swt_dv_df.loc[:, self.swt_dv_df.nunique() > ls_th]
+
+    def plot_dv_swt(self, fig_pref_str = 'swt_', fig_fmt_str = '.svg', fmt_dic = {'fontname':'Times New Roman','size': 16}):
+        plt.plot(self.pv_q_var_ser, self.swt_dv_df)
+        
+        plt.title(self.csv_file_name, **fmt_dic)
+        plt.xlabel(r"Delta Q (var)", **fmt_dic)
+        plt.ylabel(r"Delta V (V)", **fmt_dic)
+        
+        plt.grid()
+        lgd = plt.legend(list(self.swt_dv_df.columns),
+                    loc='upper left', bbox_to_anchor= (0.0, -0.22),
+                    ncol=4)
+        
+        fig_fn = fig_pref_str + os.path.splitext(self.csv_file_name)[0] + fig_fmt_str
+        fig_fpn = os.path.join(self.csv_folder_path, fig_fn)
+        plt.savefig(fig_fpn,
+                    bbox_extra_artists=(lgd,),bbox_inches='tight')
+        plt.show()
+    
+    def eval_swt_dv(self, dict_swt, flag_filter=True):
+        self.read_csv()
+        self.pre_process()
+        self.get_dv_swt(dict_swt)
+        if flag_filter:
+            self.filter_line_slope()
+        self.plot_dv_swt()
+        
+        
     @staticmethod
     def parse_volt_phasor(volt_ph_str):
         def get_float_list(ori_str):
@@ -191,10 +229,36 @@ def test_CsvExt():
     #     cur_p.read_prop_plot()
 
     # ==Demo 03 (get the price-Q curve)
-    p.read_csv()
-    p.pre_process()
-    p.get_price_q_curve(plot_flag = True)
-        
+    # p.read_csv()
+    # p.pre_process()
+    # p.get_price_q_curve(plot_flag = True)
+    
+    # ==Demo 04 (deltaV of a switch)
+    dict_swt = {
+        "RCL2": ["n264462735_1209", "n256860543_1207"],
+        "RCL7": ["n259333341_1212", "n617197553_1209"],
+        "RCL9": ["n439934984_1210", "n256904390_1209"],
+        "RCL11": ["n256834423_1212", "n616009828_1210"]
+    }
+    
+    # --Option 0 (run all .csv files in a given folder)
+    import glob
+    
+    csv_fpn_list = glob.glob(os.path.join(csv_folder_path, '*.csv'))
+    for cur_csv_fpn in csv_fpn_list:
+        cur_p = CsvExt(csv_folder_path, os.path.basename(cur_csv_fpn))
+        cur_p.eval_swt_dv(dict_swt)    
+
+    # --Option 1
+    # p.eval_swt_dv(dict_swt)
+    
+    # --Option 2
+    # p.read_csv()
+    # p.pre_process()
+    # p.get_dv_swt(dict_swt)
+    # p.filter_line_slope()
+    # p.plot_dv_swt()
+    
 
 if __name__ == "__main__":
     test_CsvExt()
